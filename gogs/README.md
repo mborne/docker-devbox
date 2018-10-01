82265bcb142ae82fb0d750ab74828329db51b707
MBorne <mborne@users.noreply.github.com>
2018-10-01 08:41:57 +0200

    gogs - backup/restore

--- gogs/README.md
+++ gogs/README.md
@@ -10,4 +10,37 @@ Docker container with gogs
 * Open http://gogs.localhost
 * Create an account (the first one will be an administrator)
 
+## Backup
+
+On host : 
+
+```bash
+bash backup.sh
+```
+
+## Restore
+
+```bash
+docker-compose down
+docker-compose run gogs /bin/bash
+```
+
+Then, in container :
+
+```bash
+cd /data
+
+mkdir -p data
+rm -fR data/*
+
+rm -fR gogs.bak
+rm -fR tmp
+mkdir -p tmp
+mkdir -p tmp/gogs-backup
+mkdir -p tmp/gogs-backup/data/
+mkdir -p tmp/gogs-backup/data/attachments
+mkdir -p tmp/gogs-backup/data/avatars
+
+USER=git /app/gogs/gogs restore -t tmp --from /backup/gogs-backup-1538373444.zip
+```
 
--- gogs/backup.sh
+++ gogs/backup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker-compose exec gogs su -c "/app/gogs/gogs backup --target=/data/backup" -s /bin/sh - git
-docker-compose exec gogs /bin/bash -c 'ls -1tr /data/backup/* | tail -n +5 | xargs /bin/rm -f'
-docker-compose exec gogs /bin/bash -c 'ls -1tr /data/backup/*'
+# fix permissions
+docker-compose exec gogs /bin/bash -c 'chown -R git:users /backup'
+# create backup
+docker-compose exec gogs su -c "/app/gogs/gogs backup --target=/backup" -s /bin/sh - git
+# keep only 3 backups
+docker-compose exec gogs /bin/bash -c 'ls -1tr /backup/* | tail -n +4 | xargs /bin/rm -f'
+docker-compose exec gogs /bin/bash -c 'ls -1tr /backup/*'
+
