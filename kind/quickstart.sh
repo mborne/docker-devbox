a50d44428b6eaf1bb3b55b4ca52a14ea7c34a1db
mborne <mborne@users.noreply.github.com>
2023-07-11 20:22:19 +0200

    feat(kind): add metric-server

--- kind/quickstart.sh
+++ kind/quickstart.sh
@@ -3,15 +3,21 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEVBOX_DIR=$(dirname $SCRIPT_DIR)
 
-echo "--------------------------------------------------------------------"
-echo "-- kind/quickstart.sh - create cluster..."
-echo "--------------------------------------------------------------------"
+echo "---------------------------------------------"
+echo "-- kind/quickstart.sh"
+echo "---------------------------------------------"
 
 bash $DEVBOX_DIR/kind/config/generate.sh | kind create cluster --config -
 
-echo "--------------------------------------------------------------------"
-echo "-- kind/quickstart.sh - install cert-manager..."
-echo "--------------------------------------------------------------------"
+#----------------------------------------
+# Install metric-server
+#----------------------------------------
+
+kubectl apply -k ${SCRIPT_DIR}/metric-server
+
+#----------------------------------------
+# Install cert-manager
+#----------------------------------------
 
 bash $DEVBOX_DIR/cert-manager/k8s-install.sh
 # create mkcert issuer if available
@@ -19,11 +25,11 @@ if which mkcert >/dev/null; then
     bash $DEVBOX_DIR/cert-manager/cluster-issuer/mkcert.sh
 fi
 
-echo "--------------------------------------------------------------------"
-echo "-- kind/quickstart.sh - install ingress controller..."
-echo "--------------------------------------------------------------------"
+#----------------------------------------
+# Install ingress controller
+#----------------------------------------
 
-DEVBOX_INGRESS=${DEVBOX_INGRESS:-traefik}
+export DEVBOX_INGRESS=${DEVBOX_INGRESS:-traefik}
 if [ "$DEVBOX_INGRESS" != "traefik" ];
 then
     NGINX_MODE=kind bash $DEVBOX_DIR/nginx-ingress-controller/k8s-install.sh
@@ -31,9 +37,10 @@ else
     TRAEFIK_MODE=kind bash $DEVBOX_DIR/traefik/k8s-install.sh
 fi
 
-echo "--------------------------------------------------------------------"
-echo "-- kind/quickstart.sh - install sample app whoami..."
-echo "--------------------------------------------------------------------"
 
-DEVBOX_INGRESS=${DEVBOX_INGRESS} TRAEFIK_MODE=kind bash $DEVBOX_DIR/whoami/k8s-install.sh
+#----------------------------------------
+# Install whoami
+#----------------------------------------
+
+bash $DEVBOX_DIR/whoami/k8s-install.sh
 
