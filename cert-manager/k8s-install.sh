b955034006cff319a619c309fcd1bba924b9539a
mborne <mborne@users.noreply.github.com>
2023-10-15 11:23:40 +0200

    chore(cert-manager): wait for CRD to be established (closes #49)

--- cert-manager/k8s-install.sh
+++ cert-manager/k8s-install.sh
@@ -12,7 +12,14 @@ helm -n cert-manager upgrade --install cert-manager \
   oci://registry-1.docker.io/bitnamicharts/cert-manager \
   --set installCRDs=true
 
-kubectl wait --namespace cert-manager \
+# Wait for cert-manager pods to be ready
+kubectl -n cert-manager wait \
     --for=condition=ready pod \
     --selector=app.kubernetes.io/name=cert-manager \
     --timeout=90s
+
+# Wait for cert-manager CRD to be ready
+kubectl wait --for condition=established --timeout=60s crd/clusterissuers.cert-manager.io
+kubectl wait --for condition=established --timeout=60s crd/issuers.cert-manager.io
+kubectl wait --for condition=established --timeout=60s crd/certificates.cert-manager.io
+
