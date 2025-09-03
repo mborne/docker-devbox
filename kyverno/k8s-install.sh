adc2499591c6c233db99d0c7d7ec2fd350f4f7c4
mborne <mborne@users.noreply.github.com>
2025-09-04 00:48:20 +0200

    fix(kyverno): update config and fix chart versions

--- kyverno/k8s-install.sh
+++ kyverno/k8s-install.sh
@@ -34,12 +34,24 @@ helm repo update
 # Create namespace kyverno if not exists
 kubectl create namespace kyverno --dry-run=client -o yaml | kubectl apply -f -
 
+# helm search repo kyverno/kyverno -l 
+# NAME                            CHART VERSION   APP VERSION
+# kyverno/kyverno                 3.5.1           v1.15.1
+# kyverno/kyverno                 3.5.0           v1.15.0
+# kyverno/kyverno                 3.4.4           v1.14.4
+# kyverno/kyverno                 3.4.3           v1.14.3
+#
+# see https://kyverno.io/docs/installation/#compatibility-matrix
+KYVERNO_VERSION=3.4.4
+
 # Install kyverno
-helm -n kyverno upgrade --install kyverno kyverno/kyverno \
+helm -n kyverno upgrade --install kyverno \
+    kyverno/kyverno --version=$KYVERNO_VERSION \
     -f "${SCRIPT_DIR}/helm/kyverno/values.yaml"
 
 # Install kyverno-policies (after due to CRDs)
-helm -n kyverno upgrade --install kyverno-policies kyverno/kyverno-policies \
+helm -n kyverno upgrade --install kyverno-policies \
+    kyverno/kyverno-policies --version=$KYVERNO_VERSION \
     -f "${SCRIPT_DIR}/helm/kyverno-policies/values.yaml"
 
 #-------------------------------------------------------------------------
@@ -52,9 +64,15 @@ helm repo add policy-reporter https://kyverno.github.io/policy-reporter
 # update helm repositories
 helm repo update
 
+# helm search repo policy-reporter
+# NAME                            CHART VERSION   APP VERSION
+# policy-reporter/policy-reporter 3.4.2           3.4.2
+POLICY_REPORTER_VERSION=3.4.2
+
 # deploy in namespace kyverno-ui
-helm -n kyverno upgrade --install policy-reporter policy-reporter/policy-reporter \
-    -f "${SCRIPT_DIR}/helm/policy-reporter/values.yaml"
+helm -n kyverno upgrade --install policy-reporter \
+  policy-reporter/policy-reporter --version=$POLICY_REPORTER_VERSION \
+  -f "${SCRIPT_DIR}/helm/policy-reporter/values.yaml"
 
 # Create Ingress with dynamic hostname for policy-reporter-ui
 cat <<EOF | kubectl -n kyverno apply -f -
